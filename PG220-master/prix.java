14c1be85b2280e8dacfac33cb2d3ebf018d01206
Cyril Hannier <channier@enseirb-matmeca.fr>
2021-11-15 09:57:02 +0100

    push

---
+++ PG220-master/prix.java
@@ -0,0 +1,17 @@
+public class Prix {
+protected int price; 
+
+public static int createprice(){
+    this(0);
+
+}
+public int createprice(int p){
+    this.price=p;
+
+}
+
+public int getprice(){
+    return price;
+}
+
+}
