f033b68ef96c1e6995d79a3090451c44246bb4bd
Yoan Echantillon <yoan.echantillon@enseirb-matmeca.fr>
2021-11-29 11:16:52 +0100

    petite modif

--- PG220-master/Etape 1/Cut_algo.java
+++ PG220-master/Etape 1/Cut_algo.java
@@ -1,4 +1,4 @@
-public class Cut_algo{
+public class Cut_algo extends Reader{
     public void main(int argv, String[] args){
         ClientOrder[] tabOrder=read_xml("clients.xml");
         int nbOrder= tabOrder.length;
@@ -7,7 +7,7 @@ public class Cut_algo{
             ClientOrder[] tabSupp = read_xml("fournisseur.xml");
             int nbSupp= tabSupp.length;
             for (int j; j<nbSupp; j++){
-                if (Compare_size(tabOrder[i],tabSupp[j])==1 && Compare_number(tabOrder[i],tabSupp[j])<0){
+                if (Compare_size(tabOrder[i],tabSupp[j])==1 && Compare_number(tabOrder[i],tabSupp[j])= >0){
                     pannel_num[i]=j;
                     break;
                 }
@@ -16,10 +16,9 @@ public class Cut_algo{
                 System.out.print("Il n'y a pas de panneau assez grand pour satisfaire la commande de la planche"+ get_plank_id(tabOrder[i])+"du client"+get_client_id(tabOrder[i]));
             }
             else{
-                cutplank
+                cut.cutplank(tabOrder[i],tabSupp[pannel_num[i]]);
                 modif_xml_fournisseur(pannel_num[i],get_number(tabOrder[i]));
             }
-
         }
     }
 }
