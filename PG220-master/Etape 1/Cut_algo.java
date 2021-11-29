4d74f384c660d5729c43ec5efb5d0f549e9420ff
Yoan Echantillon <yoan.echantillon@enseirb-matmeca.fr>
2021-11-29 10:47:43 +0100

    mise en place d'une ébauche de l'algo de découpe, rename de découpe

---
+++ PG220-master/Etape 1/Cut_algo.java
@@ -0,0 +1,25 @@
+public class Cut_algo{
+    public void main(int argv, String[] args){
+        ClientOrder[] tabOrder=read_xml("clients.xml");
+        int nbOrder= tabOrder.length;
+        int pannel_num[]=new int[nbOrder];
+        for (int i=0; i< nbOrder; i++){
+            ClientOrder[] tabSupp = read_xml("fournisseur.xml");
+            int nbSupp= tabSupp.length;
+            for (int j; j<nbSupp; j++){
+                if (Compare_size(tabOrder[i],tabSupp[j])==1 && Compare_number(tabOrder[i],tabSupp[j])<0){
+                    pannel_num[i]=j;
+                    break;
+                }
+            }
+            if (pannel_num[i]==0){
+                System.out.print("Il n'y a pas de panneau assez grand pour satisfaire la commande de la planche"+ get_plank_id(tabOrder[i])+"du client"+get_client_id(tabOrder[i]));
+            }
+            else{
+                cutplank
+                modif_xml_fournisseur(pannel_num[i],get_number(tabOrder[i]));
+            }
+
+        }
+    }
+}
