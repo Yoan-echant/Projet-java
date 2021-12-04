71da37bd628adaf54ae223ad9990625e93e44477
Cyril Hannier <channier@enseirb-matmeca.fr>
2021-12-04 17:03:27 +0100

    writer compilation ok

--- PG220-master/Etape 1/WriterXml.java
+++ PG220-master/Etape 1/WriterXml.java
@@ -5,9 +5,21 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.*;
 
+import java.io.IOException;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamWriter;
+import javax.xml.stream.XMLStreamException;
+import java.util.*;
+import java.io.*;
+import java.util.List;
+import java.util.ArrayList;
+
+
 public class WriterXml{
 
-public void ecriture(int client_id, int planche, int id_fournisseur, int panneau, int x, int y){
+public void ecriture(int [] client_id, int []planche, int [] id_fournisseur, int []panneau, int []x, int [] y){
+    try{
     FileOutputStream file = null;
     String fichier= "decoupes.xml";
     file = new FileOutputStream(fichier);
@@ -26,20 +38,20 @@ public void ecriture(int client_id, int planche, int id_fournisseur, int panneau
     for (k=0;k<taille;k++){
         writer.writeCharacters(System.getProperty("line.separator"));
         writer.writeStartElement("client");
-        writer.writeAttribute("id",id_client[k]);
-        writer.writeAttribute("planche",planche[k]);
+        writer.writeAttribute("id",Integer.toString(client_id[k]));
+        writer.writeAttribute("planche",Integer.toString(planche[k]));
         writer.writeEndElement();
 
         writer.writeCharacters(System.getProperty("line.separator"));
         writer.writeStartElement("fournisseur");
-        writer.writeAttribute("id",id_fournisseur[k]);
-        writer.writeAttribute("panneau",panneau[k]);
+        writer.writeAttribute("id",Integer.toString(id_fournisseur[k]));
+        writer.writeAttribute("panneau",Integer.toString(panneau[k]));
         writer.writeEndElement();
 
         writer.writeCharacters(System.getProperty("line.separator"));
         writer.writeStartElement("position");
-        writer.writeAttribute("x",x[k]);
-        writer.writeAttribute("y",y[k]);
+        writer.writeAttribute("x",Integer.toString(x[k]));
+        writer.writeAttribute("y",Integer.toString(y[k]));
         writer.writeEndElement();
     }
             // fin balise : 
@@ -47,6 +59,18 @@ public void ecriture(int client_id, int planche, int id_fournisseur, int panneau
     writer.writeEndElement();
     writer.writeCharacters(System.getProperty("line.separator"));
     writer.writeEndElement();
+    }
+
+     catch(IOException exc) {
+
+            System.out.print("Erreur IO: " + exc.toString());
+        }
+        catch(XMLStreamException exc)
+        {
+            System.out.print("Erreur XML: " + exc.toString());
+
+        }
+       
 
     
 
